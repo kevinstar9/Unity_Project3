d2555014eb3f21be5aaace483339c7ee0eac0f5f
kevinstar9 <109073771+kevinstar9@users.noreply.github.com>
2022-11-16 13:13:55 +0900

    16 x 16 dungeon texture added

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public float followSpeed;
+    public float Enemy_Damage;
+    public float Max_HP;
 
     // Start is called before the first frame update
     void Start()
@@ -16,14 +18,26 @@ public class EnemyController : MonoBehaviour
     void Update()
     {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player == null)
+        {
+            return;
+        }
+
         Vector3 diff = player.transform.position - transform.position;
         diff.Normalize();
         transform.position += diff * followSpeed * Time.deltaTime;
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
-            Destroy(collision.gameObject);
+        {
+            //Destroy(collision.gameObject);
+            Debug.Log(Max_HP -= Enemy_Damage);
+            if (Max_HP <= 0)
+            {
+                Destroy(collision.gameObject);
+            }
+        }
     }
 }
